168e8395f809e06c8c41ec3d8902758840cb0a04
G00414253@atu.ie <g00414253@atu.ie>
2024-10-23 15:13:09 +0100

    Added a get and delete product option

--- src/main/java/ie/atu/week6/Product.java
+++ src/main/java/ie/atu/week6/Product.java
@@ -1,5 +1,7 @@
 package ie.atu.week6;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+    @Min(value=1,message = "Must be greater or equal to one")
     private int id;
+
+    @NotBlank(message = "You must enter a name")
     private String name;
+
+    @Min(value=1,message = "Must be greater or equal to one")
     private double price;
 
 }
